71a1d9230e344b868be51d80414c9ef01b453cc9
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-14 07:17:42 +0300

    Create Insertion_sort.kt

---
+++ Sorting_algorithms/Insertion_sort.kt
@@ -0,0 +1,11 @@
+fun insertion_sort(arr: MutableList<Int>) {
+    for (i in 1 until arr.size) {
+        val key = arr[i]
+        var j = i - 1
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j]
+            j--
+        }
+        arr[j + 1] = key
+    }
+}
